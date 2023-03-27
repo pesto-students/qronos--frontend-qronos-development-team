335e234482922a7d1f259d7b54a937c8c61e6123
unknown <30varanshu@gmail.com>
2023-03-27 21:02:12 +0530

    delete entry from the database

--- src/components/dashboard/Contentview.jsx
+++ src/components/dashboard/Contentview.jsx
@@ -22,6 +22,21 @@ const Contentview = () => {
   useEffect(() => {
     getEntries()
   }, [database])
+
+  const deleteEntry = async (entry) => {
+    console.log(entry);
+    const type = entry.type.replace('Type', '')
+    console.log(type);
+    const result = await axios.delete(`http://localhost:8080/${type}/${entry._id}`, {
+      data: {
+        email: user.email,
+        database: database._id
+      }
+    })
+    console.log(result);
+    getEntries()
+  }
+
   return (
 
     <div class="relative">
@@ -88,7 +103,7 @@ const Contentview = () => {
                           entries.map((entry, index) => {
                             console.log(entry);
                             return (
-                              <tr>
+                              <tr key={index}>
                                 <td class="py-3 pr-4 border-b border-neutral-100"><input type="checkbox" /></td>
                                 <td class="py-3 pr-4 border-b border-neutral-100"><span class="text-sm">{entry.title.substr(0, 10)}</span></td>
                                 <td class="py-3 pr-4 border-b border-neutral-100" >{entry.description.substr(1, 40)}</td>
@@ -98,14 +113,13 @@ const Contentview = () => {
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                       <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                                     </svg>
-
-                                  </button> </td>
-                                <td class="py-3 pr-4 border-b border-neutral-100" >
+                                  </button>
+                                </td>
+                                <td class="py-3 pr-4 border-b border-neutral-100" onClick={e => deleteEntry(entry)} >
                                   <button> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                   </svg>
                                   </button>
-
                                 </td>
                               </tr>
                             )
